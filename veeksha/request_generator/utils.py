project-etalon/etalon

prefix based trace support

etalon/request_generator/utils.py
@@ -1,10 +1,20 @@
 import math
 import random
-from typing import List, Tuple, Union
+from abc import ABC, abstractmethod
+from typing import Any, List, Optional, Tuple, Union
 
 from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 
+from etalon.config.config import ClientConfig
+from etalon.core.request_config import RequestConfig
 from etalon.logger import init_logger
+from etalon.request_generator.length_generator.base_generator import (
+    BaseRequestLengthGenerator,
+)
+from etalon.request_generator.length_generator.trace_generator import (
+    PrefixRequestLengthGenerator,
+)
+from etalon.request_generator.utils import generate_random_prompt
 
 logger = init_logger(__name__)
 
@@ -14,6 +24,7 @@ def generate_random_prompt(
     num_prompt_tokens: int = 1024,
     num_output_tokens: int = 128,
     corpus_lines: Union[List[str], None] = None,
+    add_instruction: bool = True,
 ) -> Tuple[str, int]:
     """Generate a random prompt with a given number of tokens.
 
@@ -39,7 +50,7 @@ def generate_random_prompt(
     remaining_prompt_tokens = num_prompt_tokens - get_token_length(instruction)
     random.shuffle(corpus_lines)
     sampling_lines = True
-    prompt = instruction + '"""'
+    prompt = (instruction + '"""') if add_instruction else ""
     remaining_prompt_tokens -= get_token_length(prompt) * 2
     while sampling_lines:
         for line in corpus_lines:
@@ -53,5 +64,132 @@ def generate_random_prompt(
                 break
             prompt += line_to_add
             remaining_prompt_tokens -= get_token_length(line_to_add)
-    prompt += '"""'
+
+    if add_instruction:
+        prompt += '"""'
     return (prompt, num_prompt_tokens)
+
+
+class BaseRequestGenerator(ABC):
+
+    def __init__(
+        self,
+        client_config: ClientConfig,
+        tokenizer: Any,
+        request_length_generator: BaseRequestLengthGenerator,
+        corpus_lines: Optional[List[str]] = None,
+    ):
+        self.client_config = client_config
+        self.tokenizer = tokenizer
+        self.request_length_generator = request_length_generator
+        self.corpus_lines = corpus_lines
+
+    @abstractmethod
+    def get_request_params(
+        self,
+        request_id: Optional[int] = None,
+    ) -> RequestConfig:
+        raise NotImplementedError("Subclasses must implement generate_request method")
+
+
+class RequestGenerator(BaseRequestGenerator):
+
+    def get_request_params(
+        self,
+        request_id: Optional[int] = None,
+    ) -> RequestConfig:
+
+        (
+            num_prompt_tokens,
+            num_output_tokens,
+        ) = self.request_length_generator.get_next_num_tokens()
+        if num_prompt_tokens < 0 or num_output_tokens < 0:
+            logger.error(
+                f"Invalid number of tokens generated: prompt={num_prompt_tokens}, output={num_output_tokens} (potentially from trace request length generator)."
+            )
+        num_prompt_tokens = int(num_prompt_tokens)
+        num_output_tokens = int(num_output_tokens)
+        prompt = generate_random_prompt(
+            tokenizer=self.tokenizer,
+            num_prompt_tokens=num_prompt_tokens,
+            num_output_tokens=num_output_tokens,
+            corpus_lines=self.corpus_lines,
+        )
+        default_sampling_params = {"max_tokens": num_output_tokens}
+        default_sampling_params.update(
+            self.client_config.additional_sampling_params_dict
+        )
+        request_config = RequestConfig(
+            model=self.client_config.model,
+            prompt=prompt,
+            sampling_params=default_sampling_params,
+            llm_api=self.client_config.llm_api,
+            address_append_value=self.client_config.address_append_value,
+            id=request_id,
+        )
+
+        return request_config
+
+
+class PrefixBasedRequestGenerator(BaseRequestGenerator):
+    def __init__(
+        self,
+        client_config: ClientConfig,
+        tokenizer: Any,
+        request_length_generator: BaseRequestLengthGenerator,
+        corpus_lines: Optional[List[str]] = None,
+    ):
+        super().__init__(
+            client_config=client_config,
+            tokenizer=tokenizer,
+            request_length_generator=request_length_generator,
+            corpus_lines=corpus_lines,
+        )
+        self.past_prompts: dict[str, str] = {}
+
+    def get_request_params(
+        self,
+        request_id: Optional[int] = None,
+    ) -> RequestConfig:
+        assert isinstance(self.request_length_generator, PrefixRequestLengthGenerator)
+
+        (
+            hash_ids,
+            num_prompt_tokens,
+            num_output_tokens,
+        ) = self.request_length_generator.get_next_request_params()
+
+        prompt = '"""'
+        for hash_id in hash_ids:
+            if hash_id not in self.past_prompts:
+                prompt_segment, num_tokens = generate_random_prompt(
+                    tokenizer=self.tokenizer,
+                    num_prompt_tokens=num_prompt_tokens,
+                    num_output_tokens=num_output_tokens,
+                    corpus_lines=self.corpus_lines,
+                    add_instruction=False,
+                )
+                self.past_prompts[hash_id] = prompt_segment
+            prompt += self.past_prompts[hash_id]
+
+        prompt += '"""'
+
+        prompt += (
+            '\n\nINSTRUCTION: Mimic above text enclosed in """ quotes and generate '
+            f"long text of at least {num_output_tokens} tokens."
+        )
+
+        default_sampling_params = {"max_tokens": num_output_tokens}
+        default_sampling_params.update(
+            self.client_config.additional_sampling_params_dict
+        )
+        request_config = RequestConfig(
+            model=self.client_config.model,
+            prompt=prompt,
+            sampling_params=default_sampling_params,
+            llm_api=self.client_config.llm_api,
+            address_append_value=self.client_config.address_append_value,
+            id=request_id,
+        )
+
+        return request_config

etalon/run_benchmark.py
@@ -7,13 +7,15 @@ import time
 from multiprocessing import Queue
 from queue import Empty
 from threading import Thread
-from typing import Any, List, Optional
+from typing import Any, List
 
 from tqdm import tqdm
 
-from etalon.config.config import BenchmarkConfig, ClientConfig
+from etalon.config.config import (
+    BenchmarkConfig,
+    ClientConfig,
+)
 from etalon.core.hf_utils import get_tokenizer
-from etalon.core.request_config import RequestConfig
 from etalon.core.requests_launcher import RequestsLauncher
 from etalon.logger import init_logger
 from etalon.metrics.service_metrics import ServiceMetrics
@@ -29,46 +31,12 @@ from etalon.request_generator.length_generator.base_generator import (
 from etalon.request_generator.length_generator.generator_registry import (
     RequestLengthGeneratorRegistry,
 )
-from etalon.request_generator.utils import generate_random_prompt
+from etalon.request_generator.length_generator.trace_generator import (
+    PrefixRequestLengthGenerator,
+)
+from etalon.request_generator.utils import PrefixBasedRequestGenerator, RequestGenerator
 
 logger = init_logger(__name__)
-
-
-def get_request_params(
-    client_config: ClientConfig,
-    tokenizer: Any,
-    request_length_generator: BaseRequestLengthGenerator,
-    corpus_lines: Optional[List[str]] = None,
-    request_id: Optional[int] = None,
-) -> RequestConfig:
-    (
-        num_prompt_tokens,
-        num_output_tokens,
-    ) = request_length_generator.get_next_num_tokens()
-    if num_prompt_tokens < 0 or num_output_tokens < 0:
-        logger.error(
-            f"Invalid number of tokens generated: prompt={num_prompt_tokens}, output={num_output_tokens} (potentially from trace request length generator)."
-        )
-    num_prompt_tokens = int(num_prompt_tokens)
-    num_output_tokens = int(num_output_tokens)
-    prompt = generate_random_prompt(
-        tokenizer=tokenizer,
-        num_prompt_tokens=num_prompt_tokens,
-        num_output_tokens=num_output_tokens,
-        corpus_lines=corpus_lines,
-    )
-    default_sampling_params = {"max_tokens": num_output_tokens}
-    default_sampling_params.update(client_config.additional_sampling_params_dict)
-    request_config = RequestConfig(
-        model=client_config.model,
-        prompt=prompt,
-        sampling_params=default_sampling_params,
-        llm_api=client_config.llm_api,
-        address_append_value=client_config.address_append_value,
-        id=request_id,
-    )
-
-    return request_config
 
 
 def should_send_new_request(
@@ -94,6 +62,19 @@ def dispatch_requests(
     """Thread function to generate and dispatch requests."""
     num_errored_requests_handled = 0
 
+    request_generator_cls = (
+        PrefixBasedRequestGenerator
+        if isinstance(requests_length_generator, PrefixRequestLengthGenerator)
+        else RequestGenerator
+    )
+
+    request_generator = request_generator_cls(
+        client_config=client_config,
+        tokenizer=tokenizer,
+        request_length_generator=requests_length_generator,
+        corpus_lines=corpus_lines.copy(),
+    )
+
     while not stop_event.is_set():
         if should_send_new_request(service_metrics, num_errored_requests_handled):
             request_start_time = time.monotonic()
@@ -104,11 +85,7 @@ def dispatch_requests(
 
             # Create and dispatch request
             service_metrics.register_launched_request()
-            request_config = get_request_params(
-                client_config=client_config,
-                tokenizer=tokenizer,
-                request_length_generator=requests_length_generator,
-                corpus_lines=corpus_lines.copy(),
+            request_config = request_generator.get_request_params(
                 request_id=service_metrics.num_requests,
             )
             input_queue.put(request_config)

project-vajra/veeksha

veeksha/request_generator/utils.py
@@ -1,10 +1,20 @@
 import math
 import random
-from typing import List, Tuple, Union
+from abc import ABC, abstractmethod
+from typing import Any, List, Optional, Tuple, Union
 
 from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 
+from veeksha.config.config import ClientConfig
+from veeksha.core.request_config import RequestConfig
 from veeksha.logger import init_logger
+from veeksha.request_generator.length_generator.base_generator import (
+    BaseRequestLengthGenerator,
+)
+from veeksha.request_generator.length_generator.trace_generator import (
+    PrefixRequestLengthGenerator,
+)
+from veeksha.request_generator.utils import generate_random_prompt
 
 logger = init_logger(__name__)
 
@@ -14,6 +24,7 @@ def generate_random_prompt(
     num_prompt_tokens: int = 1024,
     num_output_tokens: int = 128,
     corpus_lines: Union[List[str], None] = None,
+    add_instruction: bool = True,
 ) -> Tuple[str, int]:
     """Generate a random prompt with a given number of tokens.
 
@@ -39,7 +50,7 @@ def generate_random_prompt(
     remaining_prompt_tokens = num_prompt_tokens - get_token_length(instruction)
     random.shuffle(corpus_lines)
     sampling_lines = True
-    prompt = instruction + '"""'
+    prompt = (instruction + '"""') if add_instruction else ""
     remaining_prompt_tokens -= get_token_length(prompt) * 2
     while sampling_lines:
         for line in corpus_lines:
@@ -53,5 +64,132 @@ def generate_random_prompt(
                 break
             prompt += line_to_add
             remaining_prompt_tokens -= get_token_length(line_to_add)
-    prompt += '"""'
+
+    if add_instruction:
+        prompt += '"""'
     return (prompt, num_prompt_tokens)
+
+
+class BaseRequestGenerator(ABC):
+
+    def __init__(
+        self,
+        client_config: ClientConfig,
+        tokenizer: Any,
+        request_length_generator: BaseRequestLengthGenerator,
+        corpus_lines: Optional[List[str]] = None,
+    ):
+        self.client_config = client_config
+        self.tokenizer = tokenizer
+        self.request_length_generator = request_length_generator
+        self.corpus_lines = corpus_lines
+
+    @abstractmethod
+    def get_request_params(
+        self,
+        request_id: Optional[int] = None,
+    ) -> RequestConfig:
+        raise NotImplementedError("Subclasses must implement generate_request method")
+
+
+class RequestGenerator(BaseRequestGenerator):
+
+    def get_request_params(
+        self,
+        request_id: Optional[int] = None,
+    ) -> RequestConfig:
+
+        (
+            num_prompt_tokens,
+            num_output_tokens,
+        ) = self.request_length_generator.get_next_num_tokens()
+        if num_prompt_tokens < 0 or num_output_tokens < 0:
+            logger.error(
+                f"Invalid number of tokens generated: prompt={num_prompt_tokens}, output={num_output_tokens} (potentially from trace request length generator)."
+            )
+        num_prompt_tokens = int(num_prompt_tokens)
+        num_output_tokens = int(num_output_tokens)
+        prompt = generate_random_prompt(
+            tokenizer=self.tokenizer,
+            num_prompt_tokens=num_prompt_tokens,
+            num_output_tokens=num_output_tokens,
+            corpus_lines=self.corpus_lines,
+        )
+        default_sampling_params = {"max_tokens": num_output_tokens}
+        default_sampling_params.update(
+            self.client_config.additional_sampling_params_dict
+        )
+        request_config = RequestConfig(
+            model=self.client_config.model,
+            prompt=prompt,
+            sampling_params=default_sampling_params,
+            llm_api=self.client_config.llm_api,
+            address_append_value=self.client_config.address_append_value,
+            id=request_id,
+        )
+
+        return request_config
+
+
+class PrefixBasedRequestGenerator(BaseRequestGenerator):
+    def __init__(
+        self,
+        client_config: ClientConfig,
+        tokenizer: Any,
+        request_length_generator: BaseRequestLengthGenerator,
+        corpus_lines: Optional[List[str]] = None,
+    ):
+        super().__init__(
+            client_config=client_config,
+            tokenizer=tokenizer,
+            request_length_generator=request_length_generator,
+            corpus_lines=corpus_lines,
+        )
+        self.past_prompts: dict[str, str] = {}
+
+    def get_request_params(
+        self,
+        request_id: Optional[int] = None,
+    ) -> RequestConfig:
+        assert isinstance(self.request_length_generator, PrefixRequestLengthGenerator)
+
+        (
+            hash_ids,
+            num_prompt_tokens,
+            num_output_tokens,
+        ) = self.request_length_generator.get_next_request_params()
+
+        prompt = '"""'
+        for hash_id in hash_ids:
+            if hash_id not in self.past_prompts:
+                prompt_segment, num_tokens = generate_random_prompt(
+                    tokenizer=self.tokenizer,
+                    num_prompt_tokens=num_prompt_tokens,
+                    num_output_tokens=num_output_tokens,
+                    corpus_lines=self.corpus_lines,
+                    add_instruction=False,
+                )
+                self.past_prompts[hash_id] = prompt_segment
+            prompt += self.past_prompts[hash_id]
+
+        prompt += '"""'
+
+        prompt += (
+            '\n\nINSTRUCTION: Mimic above text enclosed in """ quotes and generate '
+            f"long text of at least {num_output_tokens} tokens."
+        )
+
+        default_sampling_params = {"max_tokens": num_output_tokens}
+        default_sampling_params.update(
+            self.client_config.additional_sampling_params_dict
+        )
+        request_config = RequestConfig(
+            model=self.client_config.model,
+            prompt=prompt,
+            sampling_params=default_sampling_params,
+            llm_api=self.client_config.llm_api,
+            address_append_value=self.client_config.address_append_value,
+            id=request_id,
+        )
+
+        return request_config

veeksha/run_benchmark.py
@@ -7,7 +7,7 @@ import time
 from multiprocessing import Queue
 from queue import Empty
 from threading import Thread
-from typing import Any, List, Optional
+from typing import Any, List
 
 from tqdm import tqdm  # type: ignore
 
@@ -29,46 +29,11 @@ from veeksha.request_generator.length_generator.base_generator import (
 from veeksha.request_generator.length_generator.generator_registry import (
     RequestLengthGeneratorRegistry,
 )
-from veeksha.request_generator.utils import generate_random_prompt
+from veeksha.request_generator.length_generator.trace_generator import (
+    PrefixRequestLengthGenerator,
+)
 
 logger = init_logger(__name__)
-
-
-def get_request_params(
-    client_config: ClientConfig,
-    tokenizer: Any,
-    request_length_generator: BaseRequestLengthGenerator,
-    corpus_lines: Optional[List[str]] = None,
-    request_id: Optional[int] = None,
-) -> RequestConfig:
-    (
-        num_prompt_tokens,
-        num_output_tokens,
-    ) = request_length_generator.get_next_num_tokens()
-    if num_prompt_tokens < 0 or num_output_tokens < 0:
-        logger.error(
-            f"Invalid number of tokens generated: prompt={num_prompt_tokens}, output={num_output_tokens} (potentially from trace request length generator)."
-        )
-    num_prompt_tokens = int(num_prompt_tokens)
-    num_output_tokens = int(num_output_tokens)
-    prompt = generate_random_prompt(
-        tokenizer=tokenizer,
-        num_prompt_tokens=num_prompt_tokens,
-        num_output_tokens=num_output_tokens,
-        corpus_lines=corpus_lines,
-    )
-    default_sampling_params = {"max_tokens": num_output_tokens}
-    default_sampling_params.update(client_config.additional_sampling_params_dict)
-    request_config = RequestConfig(
-        model=client_config.model,
-        prompt=prompt,
-        sampling_params=default_sampling_params,
-        llm_api=client_config.llm_api,
-        address_append_value=client_config.address_append_value,
-        id=request_id,
-    )
-
-    return request_config
 
 
 def should_send_new_request(
@@ -94,6 +59,19 @@ def dispatch_requests(
     """Thread function to generate and dispatch requests."""
     num_errored_requests_handled = 0
 
+    request_generator_cls = (
+        PrefixBasedRequestGenerator
+        if isinstance(requests_length_generator, PrefixRequestLengthGenerator)
+        else RequestGenerator
+    )
+
+    request_generator = request_generator_cls(
+        client_config=client_config,
+        tokenizer=tokenizer,
+        request_length_generator=requests_length_generator,
+        corpus_lines=corpus_lines.copy(),
+    )
+
     while not stop_event.is_set():
         if should_send_new_request(service_metrics, num_errored_requests_handled):
             request_start_time = time.monotonic()
@@ -104,11 +82,7 @@ def dispatch_requests(
 
             # Create and dispatch request
             service_metrics.register_launched_request()
-            request_config = get_request_params(
-                client_config=client_config,
-                tokenizer=tokenizer,
-                request_length_generator=requests_length_generator,
-                corpus_lines=corpus_lines.copy(),
+            request_config = request_generator.get_request_params(
                 request_id=service_metrics.num_requests,
             )
             input_queue.put(request_config)