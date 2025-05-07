c992691d7888156507d0c1d365e46860e725950b
chus-chus <jemanac1998@gmail.com>
2025-05-06 21:27:55 -0400

    minor

--- veeksha/capacity_search/capacity_search.py
+++ veeksha/capacity_search/capacity_search.py
@@ -471,9 +471,9 @@ class CapacitySearch:
                             logger.error(f"Error freeing port: {str(e)}")
                             port = port + 1
 
-                    print("Server env: ", self.args.server_env)
-                    if self.args.server_env:
-                        cmd = ["mamba", "run", "-p", self.args.server_env] + cmd
+                    print("Server env: ", self.job_config.server_config.server_env)
+                    if self.job_config.server_config.server_env:
+                        cmd = ["mamba", "run", "-p", self.job_config.server_config.server_env] + cmd
 
                     print(f"Final command: {' '.join(cmd)}")
                     print(f"Starting server process on port {port}")
