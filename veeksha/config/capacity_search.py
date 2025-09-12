5bc71147bc7a563383427b898d96d78cddde68ac
chus-chus <jemanac1998@gmail.com>
2025-09-12 12:34:52 -0400

    minor

--- veeksha/config/capacity_search.py
+++ veeksha/config/capacity_search.py
@@ -39,7 +39,7 @@ class CapacitySearchConfig:
         metadata={"help": "Maximum number of iterations for capacity search."},
     )
     output_dir: str = field(
-        default="./veeksha/capacity_search/output",
+        default="capsearch_results",
         metadata={
             "help": "Output directory for capacity search benchmarks and results. Overrides output_dir in benchmark_config.metrics_config."
         },
